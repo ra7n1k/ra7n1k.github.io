b04853ac03b6fdf97ae4aa13fc587836bb3dbdc7
yakitori-server-admin <civilization.destruction@gmail.com>
2023-08-24 00:47:30 +0900

    画像の修正とスクリプト削除 - ホームの画像に圧縮されたファイル拡張子オプションを追加しました - 不要なスクリプトファイルを削除しました

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const menu = document.getElementById("menu");
-const wrapper = document.getElementById("wrapper");
-
-menu.addEventListener("click", function () {
-  wrapper.classList.toggle("display");
-});
